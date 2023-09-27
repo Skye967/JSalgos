3ad2748c7cc59f0c5ee8a0a04b06c04684d15fde
AKAbigdaddy <akabigdaddy@skyes-MBP.home>
2023-09-27 08:20:33 -0600

    greedIsGood

--- greedIsGood.js
+++ greedIsGood.js
@@ -14,7 +14,7 @@ function score(dice) {
     for (let i = 0; i < arr.length; i++) {
         console.log(arr[i]);
 
-        if (arr.filter(x => x == arr[i]).length >= 3) {
+        if (arr.filter(x => x == arr[i]).length >= 3 && i <= 3) {
             let index = arr.indexOf(arr[i], 0);
 
             switch (arr[i]) {
@@ -55,7 +55,7 @@ function score(dice) {
     return score
 }
 
-console.log(score([2, 3, 4, 6, 2]));
+console.log(score([3,3,3,3,3]));
 console.log(score([4, 4, 4, 3, 3]));
 console.log(score([2, 4, 4, 5, 4]));
 console.log(score([1,1,1, 3, 3]));
